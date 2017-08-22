c5eeda33fd6d9affddcc2afe8b4c194d88b4ff2c
Todd Szakacs <tszakacs@msn.com>
2017-08-21 22:55:50 -0400

    styling updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
 		$("#bandButtons").empty();
 		  for (var i = 0; i < topics.length; i++) {
 		  	var gifButton = $("<button>");
-		  	  gifButton.addClass("band");
-		  	  gifButton.attr("data-name", topics[i]);
-		  	  gifButton.text(topics[i]);
+		  	    gifButton.addClass("band");
+		  	    gifButton.attr("data-name", topics[i]);
+		  	    gifButton.text(topics[i]);
 
 		$("#bandButtons").append(gifButton);
 	}
@@ -17,31 +17,26 @@ $(document).ready(function() {
 
 $(document).on("click", ".band", function() {
 	var b = $(this).html();
-	console.log(b);
 
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + b + "&api_key=80c2d6bffdf5475bb0b4b08fe44bf797&limit=10";
-		console.log(queryURL);
 		$.ajax({
 			url: queryURL,
 			method: "GET"
 		})
 		.done(function(response) {
 			var results = response.data;
-				console.log(results);
 
 			$("#gifsArea").empty();
 		  		for (var j = 0; j < results.length; j++) {
 		  		  var gifDiv = $("<div>");
 		  		  var gifImage = results[j].images.fixed_height.url;
 		  		  var still = results[j].images.fixed_height_still.url;
-		  			  console.log(gifImage);
 		  		  var bandImage = $("<img>").attr("src", still).attr("data-animate", gifImage).attr("data-still", still);
 		  		  bandImage.attr("data-state", "still");
 			$("#gifsArea").prepend(bandImage);
 		 	bandImage.on("click", playsGif);
 
 		 		  var rating = results[j].rating;
-		 			  console.log(rating);
 		 		  var showRating = $("<p>").text("Rating: " + rating);
 			$("#gifsArea").prepend(showRating);
 
@@ -51,7 +46,7 @@ $(document).on("click", ".band", function() {
 
 		function playsGif() {
 			var state = $(this).attr("data-state");
-			    console.log(state);
+			    
 			if (state == "still") {
 				$(this).attr("src", $(this).data("animate"));
 				$(this).attr("data-state", "animate");
@@ -64,19 +59,20 @@ $(document).on("click", ".band", function() {
 	})
 
 	 	$(document).on("click", "#addBand", function() {
-    		if ($("#addBands-input").val().trim() == "") {
-      		  alert("Entry cannot be blank");
-   			} else {
+	 		if ($("#addBands-input").val().trim() == "") {
+    		  console.log(" ");
+	 		} else {
     		var bands = $("#addBands-input").val().trim();
     		topics.push(bands);
     		$("#addBands-input").val("");
     		showBandsButtons();
     		return false;
-    		}
+    	}
+    });
 
     });
 
-	});
+	
 
 
 
